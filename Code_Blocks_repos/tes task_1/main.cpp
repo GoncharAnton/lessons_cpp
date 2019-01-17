ea79c61f8a989d2c70eb18bacbb63636fe4ef336
Gonchar_Anton <meh.alyans@gmail.com>
2019-01-18 00:24:28 +0200

    hello code

---
+++ Code_Blocks_repos/tes task_1/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+/*
+void hi()
+{
+    std::cout << "hello world!";
+}*/
+/*
+void hi()
+{
+    std::cout << "hello \n" << "world \n";
+}*/
+void hi()
+{
+    int a = 12892323 + 454665768;
+    std::cout << "a = " << a;
+}
+int main()
+{
+    hi();
+    return 0;
+}
